cabfd159208ea9bf06e36fb3b6f485de31e4252d
Amanda Guerra <guerraamanda1@hotmail.com>
2022-05-06 20:44:18 -0300

    Adicionando sons e corrigindo erros

--- js/index.js
+++ js/index.js
@@ -8,8 +8,12 @@ let minutesDisplay = document.querySelector('.minutes')
 let secondsDisplay = document.querySelector('.seconds')
 let minutes = Number(minutesDisplay.textContent)
 let timerTimeOut
+let buttonForest = document.querySelector('.forest')
+let buttonRain = document.querySelector('.rain')
+let buttonCoffee = document.querySelector('.coffee')
+let buttonFireplace = document.querySelector('.fireplace')
 
-import Sounds from "./sounds.js"
+import Sounds from './sounds.js'
 
 const sounds = Sounds()
 
@@ -33,48 +37,51 @@ function resetTimer() {
 }
 
 function countdown() {
-  timerTimeOut = setTimeout( function() {
+  timerTimeOut = setTimeout(function () {
     let minutes = Number(minutesDisplay.textContent)
     let seconds = Number(secondsDisplay.textContent)
 
-    if (minutes <= 0 && seconds <=0) {
+    if (minutes <= 0 && seconds <= 0) {
       updateDisplay()
       resetControls()
+      sounds.endTimer()
       return
     }
 
     if (seconds <= 0) {
-      seconds = 7
+      seconds = 60
       --minutes
     }
-  
-     updateDisplay(minutes, (seconds- 1))
+
+    updateDisplay(minutes, seconds - 1)
 
     countdown()
   }, 1000)
 }
 
-
-buttonPlay.addEventListener('click', function() {
+buttonPlay.addEventListener('click', function () {
   buttonPlay.classList.add('hide')
   buttonPause.classList.remove('hide')
   buttonStop.classList.remove('hide')
   buttonSet.classList.add('hide')
+  sounds.pressButton()
   countdown()
 })
 
-buttonPause.addEventListener('click', function() {
+buttonPause.addEventListener('click', function () {
   buttonPause.classList.add('hide')
   buttonPlay.classList.remove('hide')
+  sounds.pressButton()
   clearTimeout(timerTimeOut)
 })
 
-buttonStop.addEventListener('click', function(){
+buttonStop.addEventListener('click', function () {
+  sounds.pressButton()
   resetControls()
   resetTimer()
-} )
+})
 
-buttonSet.addEventListener('click', function() {
+buttonSet.addEventListener('click', function () {
   let newMinutes = Number(prompt('Quantos minutos?'))
   if (!newMinutes) {
     resetTimer()
@@ -82,4 +89,84 @@ buttonSet.addEventListener('click', function() {
   }
   minutes = newMinutes
   updateDisplay(minutes, 0)
+})
+
+buttonPlus.addEventListener('click', function () {
+  minutesDisplay.textContent = String(minutes + 5).padStart(2, '0')
+  minutes = Number(minutesDisplay.textContent)
+  if (minutes <= 0) {
+    updateDisplay(0, 0)
+  }
+  sounds.pressButton()
+})
+
+buttonMinus.addEventListener('click', function () {
+  minutesDisplay.textContent = String(minutes - 5).padStart(2, '0')
+  minutes = Number(minutesDisplay.textContent)
+  if (minutes <= 0) {
+    minutes = 0
+    updateDisplay(0, 0)
+  }
+  sounds.pressButton()
+})
+
+function turnItOff(button) {
+  if (button.classList.contains('active')) {
+    button.classList.remove('active')
+  }
+}
+
+function music() {
+  if (buttonForest.classList.contains('active')) {
+    sounds.forestAudio.play()
+  } else {
+    sounds.forestAudio.pause()
+  }
+  if (buttonRain.classList.contains('active')) {
+    sounds.rainAudio.play()
+  } else {
+    sounds.rainAudio.pause()
+  }
+  if (buttonCoffee.classList.contains('active')) {
+    sounds.coffeeAudio.play()
+  } else {
+    sounds.coffeeAudio.pause()
+  }
+  if (buttonFireplace.classList.contains('active')) {
+    sounds.fireplaceAudio.play()
+  } else {
+    sounds.fireplaceAudio.pause()
+  }
+}
+
+buttonForest.addEventListener('click', function () {
+  buttonForest.classList.toggle('active')
+  turnItOff(buttonCoffee)
+  turnItOff(buttonRain)
+  turnItOff(buttonFireplace)
+  music()
+})
+
+buttonRain.addEventListener('click', function () {
+  buttonRain.classList.toggle('active')
+  turnItOff(buttonCoffee)
+  turnItOff(buttonForest)
+  turnItOff(buttonFireplace)
+  music()
+})
+
+buttonCoffee.addEventListener('click', function () {
+  buttonCoffee.classList.toggle('active')
+  turnItOff(buttonRain)
+  turnItOff(buttonForest)
+  turnItOff(buttonFireplace)
+  music()
+})
+
+buttonFireplace.addEventListener('click', function () {
+  buttonFireplace.classList.toggle('active')
+  turnItOff(buttonCoffee)
+  turnItOff(buttonForest)
+  turnItOff(buttonRain)
+  music()
 })
--- js/sounds.js
+++ js/sounds.js
@@ -4,8 +4,24 @@ export default function Sounds() {
   const rainAudio = document.querySelector('.rainAudio')
   const coffeeAudio = document.querySelector('.coffeeAudio')
   const fireplaceAudio = document.querySelector('.fireplaceAudio')
+  const buttonPressAudio = new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/button-press.wav?raw=true'
+  )
+  const kitchenTimer = new Audio(
+    'https://github.com/maykbrito/automatic-video-creator/blob/master/audios/kichen-timer.mp3?raw=true'
+  )
+
+  function pressButton () {
+    buttonPressAudio.play()
+  }
+
+  function endTimer () {
+    kitchenTimer.play()
+  }
 
   return {
+    endTimer,
+    pressButton,
     forestAudio,
     rainAudio,
     coffeeAudio,
